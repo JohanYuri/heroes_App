c53a3bc93c7d940ffec569af553154b1ff6f9ce3
Johan Yuri Martínez García <johanyuri24@gmail.com>
2025-10-24 02:43:51 -0600

    13-009 Interfaz Héroe COMPLETADO

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,24 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
+import { Heroe } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-listado',
-  template: `
-    <p>
-      listado works!
-    </p>
-  `,
+  templateUrl: './listado.component.html',
   styles: [
   ]
 })
 export class ListadoComponent implements OnInit {
 
+  heroes: Heroe[] = [];
+
   constructor( private heroesService: HeroesService) { }
 
 
   ngOnInit(): void {
-    this.heroesService.getHeroes().subscribe( resp => {
-      console.log(resp);
+    this.heroesService.getHeroes().subscribe( heroes => {
+      this.heroes = heroes;
     });
   }
 
